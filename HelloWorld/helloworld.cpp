1b6c05c739d7bdfed921375d6fdc8ca89ac1d889
write2maka <write2maka@gmail.com>
2021-08-29 12:56:34 -0700

    First commit

---
+++ HelloWorld/helloworld.cpp
@@ -0,0 +1,16 @@
+/*
+ * helloworld.cpp
+ *
+ *  Created on: Aug 29, 2021
+ *      Author: maaya
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello world" << endl;
+
+	return 0;
+}
